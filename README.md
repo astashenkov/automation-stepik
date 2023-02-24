2de84854ec9f85b90b84c3986477a030312367a6
astashenkov <ya.astashenkov@gmail.com>
2023-02-24 16:29:59 +0300

    Add my firs script

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 https://stepik.org/course/575/
 
 This is a repository where I publish my solutions to problems from the Stepik automation QA course.
+In this course, I'm master Selenium and take the first steps in the automation testing.
---
+++ lesson1_step2_first_run_browser.py
@@ -0,0 +1,16 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+time.sleep(5)
+driver.get('https://suninjuly.github.io/text_input_task.html')
+time.sleep(5)
+textarea = driver.find_element(By.CSS_SELECTOR, '.textarea')
+textarea.send_keys('Hello!')
+time.sleep(5)
+submit_button = driver.find_element(By.CSS_SELECTOR, '.submit-submission')
+submit_button.click()
+time.sleep(5)
+driver.quit()
