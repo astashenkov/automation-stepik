700b849fa163b5f94da4057a1ef4cc44f5508409
astashenkov <ya.astashenkov@gmail.com>
2023-02-25 21:49:26 +0300

    Finished section number 2

---
+++ section2/lesson3_step4.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+
+def calc(value: int) -> str:
+    return str(math.log(abs(12 * math.sin(int(value)))))
+
+
+link = 'http://suninjuly.github.io/alert_accept.html'
+
+browser = webdriver.Chrome()
+browser.get(link)
+
+browser.find_element(By.CSS_SELECTOR, '[type="submit"]').click()
+browser.switch_to.alert.accept()
+
+x = int(browser.find_element(By.ID, 'input_value').text)
+browser.find_element(By.ID, 'answer').send_keys(str(calc(x)))
+browser.find_element(By.CSS_SELECTOR, '.btn.btn-primary').click()
+
+time.sleep(5)
+browser.quit()
