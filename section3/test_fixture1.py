74259ff08f85ab63af642f43223aad427f90fa7a
astashenkov <ya.astashenkov@gmail.com>
2023-02-26 21:09:32 +0300

    Finished lesson 4 from section 3

---
+++ section3/test_fixture1.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+class TestMainPage1:
+
+    @classmethod
+    def setup_class(cls):
+        print("\nstart browser for test suite..")
+        cls.browser = webdriver.Chrome()
+
+    @classmethod
+    def teardown_class(cls):
+        print("\nquit browser for test suite..")
+        cls.browser.quit()
+
+    def test_guest_should_see_login_link(self):
+        self.browser.get(link)
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self):
+        self.browser.get(link)
+        self.browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+
+
+class TestMainPage2:
+
+    def setup_method(self):
+        print("\nstart browser for test..")
+        self.browser = webdriver.Chrome()
+
+    def teardown_method(self):
+        print("\nquit browser for test..")
+        self.browser.quit()
+
+    def test_guest_should_see_login_link(self):
+        self.browser.get(link)
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self):
+        self.browser.get(link)
+        self.browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
