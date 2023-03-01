ec4a38f1149f296b929f55b68588a96ca9da867e
astashenkov <ya.astashenkov@gmail.com>
2023-03-01 21:26:09 +0300

    Make solution of the final task in the third section

---
+++ section3/final-task/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='ru',
+                     help="Choose website language")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': browser_language})
+    browser = webdriver.Chrome(options=options)
+    print("\nStart browser for test..")
+    yield browser
+    print("\nQuit browser..")
+    browser.quit()
---
+++ section3/final-task/test_items.py
@@ -0,0 +1,15 @@
+import selenium
+from selenium.webdriver.common.by import By
+
+def test_exists_add_to_cart_button(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    browser.get(link)
+
+    try:
+        text_on_the_button = browser.find_element(
+            By.CSS_SELECTOR, '.btn-add-to-basket'
+        ).text
+    except selenium.common.exceptions.NoSuchElementException:
+        text_on_the_button = ''
+
+    assert text_on_the_button, "Didn't find button"
